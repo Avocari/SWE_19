4431db3782c1cce6dbd860aefc72066d11e5b299
Clemens Walluschek <official.avocari@gmail.com>
2019-03-09 11:33:28 +0100

    Added Dummy Clock methods and tests for functionality

--- app/src/test/java/at/tugraz/ist/swe/stopwatch/ClockUnitTest.java
+++ app/src/test/java/at/tugraz/ist/swe/stopwatch/ClockUnitTest.java
@@ -1,13 +1,21 @@
 package at.tugraz.ist.swe.stopwatch;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ClockUnitTest {
-	Clock clock = new Clock();
+	Clock clock;
+
+	@Before
+	public void setUp() throws Exception {
+		clock = new Clock();
+	}
 
 	@Test
 	public void testInitialState() {
@@ -15,5 +23,21 @@ public class ClockUnitTest {
 		assertFalse(clock.isRunning());
 		assertEquals(0L, clock.getElapsedTime());
 		assertEquals("0:00:00", clock.getElapsedTimeString());
+	}
+
+	@Test
+	public void testStartSetsRunning() {
+		clock.start();
+
+		assertTrue(clock.isRunning());
+	}
+
+	@Test
+	public void testStartElapsesTime() throws InterruptedException {
+		clock.start();
+		Thread.sleep(100);
+
+		assertNotEquals(0, clock.getElapsedTime());
+		assertNotEquals("0:00:00", clock.getElapsedTimeString());
 	}
 }
