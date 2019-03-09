e3437cfaa8a9abc9a4335c7ae65f13e2de6d6031
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2019-03-09 10:54:24 +0100

    Added buttons

--- app/src/androidTest/java/at/tugraz/ist/swe/stopwatch/MainActivityEspressoTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/stopwatch/MainActivityEspressoTest.java
@@ -2,8 +2,10 @@ package at.tugraz.ist.swe.stopwatch;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,6 +22,9 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
+
+	@Rule
+	public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
 	@Test
 	public void testButtonsVisible() {
